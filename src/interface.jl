d964ae14c20479d0d548ed5ebba1a92df1e6e795
Takuya Iwanaga <takuyai@gmail.com>
2023-11-24 19:52:14 +1100

    Tentative support for overlaid heatmaps

--- src/interface.jl
+++ src/interface.jl
@@ -148,3 +148,7 @@ function iviz(f, data::Continuous2D)
   onany(update, resolution, axislimits)
   FigureAxisPlotEx(fap, () -> update(resolution[], axislimits[]), nothing)
 end
+
+
+iheatmap!(g::Axis, z::AbstractMatrix; kwargs...) = iviz((x, y, z) -> heatmap!(g, x, y, z; kwargs...), Samples2D(1:size(z,1), 1:size(z,2), z))
+iheatmap!(g::FigureAxisPlotEx, z::AbstractMatrix; kwargs...) = iviz((x, y, z) -> heatmap!(g.axis, x, y, z; kwargs...), Samples2D(1:size(z,1), 1:size(z,2), z))
