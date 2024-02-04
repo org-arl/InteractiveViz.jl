b9e5e516c5e3cb45bd0a957a45f656ec6e3cecd0
Takuya Iwanaga <takuyai@gmail.com>
2024-02-04 18:55:39 +1100

    Standardize to two space indent

--- src/interface.jl
+++ src/interface.jl
@@ -2,32 +2,31 @@ using GeometryBasics
 using FillArrays
 using Makie
 
-
 ### figure-axis-plot container
 
 struct FigureAxisPlotEx{T}
-    fap::T
-    update::Function
-    params::Union{Nothing, Dict{Symbol, Any}}
+  fap::T
+  update::Function
+  params::Union{Nothing, Dict{Symbol, Any}}
 end
 
 function Base.getproperty(fapd::FigureAxisPlotEx, sym::Symbol)
-    sym === :figure && return fapd.fap.figure
-    sym === :axis && return fapd.fap.axis
-    sym === :plot && return fapd.fap.plot
-    return getfield(fapd, sym)
+  sym === :figure && return fapd.fap.figure
+  sym === :axis && return fapd.fap.axis
+  sym === :plot && return fapd.fap.plot
+  return getfield(fapd, sym)
 end
 
 Base.display(fapd::FigureAxisPlotEx; kwargs...) = display(fapd.fap; kwargs...)
 Base.display(screen::MakieScreen, fapd::FigureAxisPlotEx; kwargs...) = display(screen, fapd.fap; kwargs...)
 
 function Base.show(io::IO, fapd::FigureAxisPlotEx)
-    fapd.fap isa Makie.FigureAxisPlot && display(fapd.fap)
-    return show(io, fapd.fap)
+  fapd.fap isa Makie.FigureAxisPlot && display(fapd.fap)
+  return show(io, fapd.fap)
 end
 
 """
-    repaint(fapd::FigureAxisPlotEx)
+  repaint(fapd::FigureAxisPlotEx)
 
 Repaint a figure to update data that might have changed in the data source.
 """
@@ -40,116 +39,115 @@ repaint(fapd::FigureAxisPlotEx) = fapd.update()
 # updates, as the scene is redrawn for every movement causing image tearing.
 # Instead, we artificially limit the number of refreshes that occur.
 
-
 function iviz(f, data::PointSet)
-    lims = limits(data)
-    xrange = range(lims[1], lims[2]; length=2)
-    yrange = range(lims[3], lims[4]; length=2)
+  lims = limits(data)
+  xrange = range(lims[1], lims[2]; length=2)
+  yrange = range(lims[3], lims[4]; length=2)
+  qdata = sample(data, xrange, yrange)
+  pts = Observable(qdata.points)
+  fap = f(pts)
+  if current_axis().limits[] == (nothing, nothing)
+    xlims!(current_axis(), lims[1], lims[2])
+    ylims!(current_axis(), lims[3], lims[4])
+  end
+
+  reset_limits!(current_axis())
+
+  function update(res, lims)
+    xrange = range(lims.origin[1], lims.origin[1] + lims.widths[1]; length=round(Int, res[1]))
+    yrange = range(lims.origin[2], lims.origin[2] + lims.widths[2]; length=round(Int, res[2]))
     qdata = sample(data, xrange, yrange)
-    pts = Observable(qdata.points)
-    fap = f(pts)
-    if current_axis().limits[] == (nothing, nothing)
-        xlims!(current_axis(), lims[1], lims[2])
-        ylims!(current_axis(), lims[3], lims[4])
+    return pts[] = qdata.points
+  end
+
+  resolution = current_axis().scene.camera.resolution
+  axislimits = current_axis().finallimits
+  update(resolution[], axislimits[])
+
+  onany(resolution, axislimits) do res, axlimits
+    if @isdefined(redraw_limit)
+      close(redraw_limit)
     end
+    redraw_limit = Timer(x -> update(res, axlimits), 0.1)
+  end
 
-    reset_limits!(current_axis())
-
-    function update(res, lims)
-        xrange = range(lims.origin[1], lims.origin[1] + lims.widths[1]; length=round(Int, res[1]))
-        yrange = range(lims.origin[2], lims.origin[2] + lims.widths[2]; length=round(Int, res[2]))
-        qdata = sample(data, xrange, yrange)
-        return pts[] = qdata.points
-    end
-
-    resolution = current_axis().scene.camera.resolution
-    axislimits = current_axis().finallimits
-    update(resolution[], axislimits[])
-
-    onany(resolution, axislimits) do res, axlimits
-        if @isdefined(redraw_limit)
-            close(redraw_limit)
-        end
-        redraw_limit = Timer(x -> update(res, axlimits), 0.1)
-    end
-
-    return FigureAxisPlotEx(fap, () -> update(resolution[], axislimits[]), nothing)
+  return FigureAxisPlotEx(fap, () -> update(resolution[], axislimits[]), nothing)
 end
 
 function iviz(f, data::Continuous1D)
-    lims = limits(data)
-    r = range(lims[1], lims[2]; length=2)
-    qdata = sample(data, r, nothing)
-    x = Observable(qdata.x)
-    y = Observable(qdata.y)
-    fap = f(x, y)
+  lims = limits(data)
+  r = range(lims[1], lims[2]; length=2)
+  qdata = sample(data, r, nothing)
+  x = Observable(qdata.x)
+  y = Observable(qdata.y)
+  fap = f(x, y)
 
-    if current_axis().limits[] == (nothing, nothing)
-        xlims!(current_axis(), lims[1], lims[2])
+  if current_axis().limits[] == (nothing, nothing)
+    xlims!(current_axis(), lims[1], lims[2])
+  end
+
+  reset_limits!(current_axis())
+
+  function update(res, lims)
+    xrange = range(lims.origin[1], lims.origin[1] + lims.widths[1]; length=round(Int, res[1]))
+    yrange = range(lims.origin[2], lims.origin[2] + lims.widths[2]; length=round(Int, res[2]))
+    qdata = sample(data, xrange, yrange)
+    x.val = qdata.x
+    return y[] = qdata.y
+  end
+
+  resolution = current_axis().scene.camera.resolution
+  axislimits = current_axis().finallimits
+  update(resolution[], axislimits[])
+
+  onany(resolution, axislimits) do res, axlimits
+    if @isdefined(redraw_limit)
+      close(redraw_limit)
     end
+    redraw_limit = Timer(x -> update(res, axlimits), 0.1)
+  end
 
-    reset_limits!(current_axis())
-
-    function update(res, lims)
-        xrange = range(lims.origin[1], lims.origin[1] + lims.widths[1]; length=round(Int, res[1]))
-        yrange = range(lims.origin[2], lims.origin[2] + lims.widths[2]; length=round(Int, res[2]))
-        qdata = sample(data, xrange, yrange)
-        x.val = qdata.x
-        return y[] = qdata.y
-    end
-
-    resolution = current_axis().scene.camera.resolution
-    axislimits = current_axis().finallimits
-    update(resolution[], axislimits[])
-
-    onany(resolution, axislimits) do res, axlimits
-        if @isdefined(redraw_limit)
-            close(redraw_limit)
-        end
-        redraw_limit = Timer(x -> update(res, axlimits), 0.1)
-    end
-
-    return FigureAxisPlotEx(fap, () -> update(resolution[], axislimits[]), nothing)
+  return FigureAxisPlotEx(fap, () -> update(resolution[], axislimits[]), nothing)
 end
 
 function iviz(f, data::Continuous2D)
-    lims = limits(data)
-    rx = range(lims[1], lims[2]; length=2)
-    ry = range(lims[3], lims[4]; length=2)
-    qdata = sample(data, rx, ry)
-    x = Observable(qdata.x)
-    y = Observable(qdata.y)
-    z = Observable(qdata.z)
-    fap = f(x, y, z)
+  lims = limits(data)
+  rx = range(lims[1], lims[2]; length=2)
+  ry = range(lims[3], lims[4]; length=2)
+  qdata = sample(data, rx, ry)
+  x = Observable(qdata.x)
+  y = Observable(qdata.y)
+  z = Observable(qdata.z)
+  fap = f(x, y, z)
 
-    if current_axis().limits[] == (nothing, nothing)
-        xlims!(current_axis(), lims[1], lims[2])
-        ylims!(current_axis(), lims[3], lims[4])
+  if current_axis().limits[] == (nothing, nothing)
+    xlims!(current_axis(), lims[1], lims[2])
+    ylims!(current_axis(), lims[3], lims[4])
+  end
+
+  reset_limits!(current_axis())
+
+  function update(res, lims)
+    xrange = range(lims.origin[1], lims.origin[1] + lims.widths[1]; length=round(Int, res[1]))
+    yrange = range(lims.origin[2], lims.origin[2] + lims.widths[2]; length=round(Int, res[2]))
+    qdata = sample(data, xrange, yrange)
+    x.val = qdata.x
+    y.val = qdata.y
+    return z[] = qdata.z
+  end
+
+  resolution = current_axis().scene.camera.resolution
+  axislimits = current_axis().finallimits
+  update(resolution[], axislimits[])
+
+  onany(resolution, axislimits) do res, axlimits
+    if @isdefined(redraw_limit)
+      close(redraw_limit)
     end
+    redraw_limit = Timer(x -> update(res, axlimits), 0.1)
+  end
 
-    reset_limits!(current_axis())
-
-    function update(res, lims)
-        xrange = range(lims.origin[1], lims.origin[1] + lims.widths[1]; length=round(Int, res[1]))
-        yrange = range(lims.origin[2], lims.origin[2] + lims.widths[2]; length=round(Int, res[2]))
-        qdata = sample(data, xrange, yrange)
-        x.val = qdata.x
-        y.val = qdata.y
-        return z[] = qdata.z
-    end
-
-    resolution = current_axis().scene.camera.resolution
-    axislimits = current_axis().finallimits
-    update(resolution[], axislimits[])
-
-    onany(resolution, axislimits) do res, axlimits
-        if @isdefined(redraw_limit)
-            close(redraw_limit)
-        end
-        redraw_limit = Timer(x -> update(res, axlimits), 0.1)
-    end
-
-    return FigureAxisPlotEx(fap, () -> update(resolution[], axislimits[]), nothing)
+  return FigureAxisPlotEx(fap, () -> update(resolution[], axislimits[]), nothing)
 end
 
 ### interface
